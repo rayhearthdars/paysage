c43cd20ce03b28e622e85000575d04b65f3379e0
gabriellebrie <104355268+gabriellebrie@users.noreply.github.com>
2022-10-16 14:08:28 +0200

    Add files via upload

--- essai.js
+++ essai.js
@@ -4,11 +4,9 @@ let p2 = {x: 475, y: 400};
 let p3 = {x: 475, y: 450};
 let p4 = {x: 700, y: 600};
 let p5 = {x: 800, y: 600};
-
 //points of reference for the ground left side of the road
 let p6 = {x: 0, y: 600};
 let p7 = {x: 0, y: 450};
-
 //big tree
 let trunk = {
 	x: 660,
@@ -24,7 +22,6 @@ let branch = {
 	x3: 662,
 	y3: 100
 }
-
 //small trees
 let smallbranch = {
 	x1: 750,
@@ -34,7 +31,6 @@ let smallbranch = {
 	x3: 760,
 	y3:285
 }
-
 //clouds
 let x = 0
 let cloud1 = {
@@ -43,24 +39,50 @@ let cloud1 = {
 	height: 100
 }
 
-let variationclouds1x, variationClouds2x, translateCharacterX, characterPosition, translateCharacterY;
+let variationclouds1x, variationClouds2x, characterPositionX, characterScale, characterPositionY;
 
 function setup(){
 	createCanvas(800, 600);
 	variationclouds1x = 0;
 	variationClouds2x = 300;
 	translateCharacterX = 520;
-	translateCharacterY = 600; 
-	characterPosition = 1;
-
+	characterPositionY = 600; 
+	characterScale = 1;
 }
 
 function draw(){
-
 	noStroke();
 	background(149, 206, 208);
 
-//draw some clouds
+	drawCloud1();
+	drawCloud2();
+	drawFirstMountainLeft();
+	drawSecondMountainLeft();
+	drawBigTree();
+	drawSmallTreesBehind();
+	rightSideOfTheRoad();
+	leftSideOfTheRoad();
+	drawRoad();
+	scaleCharacterSmaller();
+}
+
+
+function mouseClicked () {
+	fill(250, 250, 250);
+	translateCharacterX -= 10;
+	if (translateCharacterX <= 400){
+		translateCharacterX = 400;
+	}
+	characterPositionY -= 10;
+	if (characterPositionY <= 400){
+		characterPositionY = 400;
+	}
+	characterScale = constrain(characterScale, 0.2, 1);
+	characterScale -= 0.01;
+}
+
+
+function drawCloud2(){
 	variationClouds2x +=  1;
 	constrain(variationClouds2x, 0, 800);
 	if (variationClouds2x === 800){
@@ -73,19 +95,9 @@ function draw(){
 	ellipse(variationClouds2x + 250, cloud1.y - 50, cloud1.width, cloud1.height);
 	ellipse(variationClouds2x + 300, cloud1.y - 50, cloud1.width - 25, cloud1.height - 25);
 	ellipse(variationClouds2x + 350, cloud1.y - 50, cloud1.width - 50, cloud1.height - 50);
+}
 
-//draw second mountain on the left
-	fill(17, 48, 16);
-	beginShape();
-		vertex((p2.x - 250 + p4.x - 650) / 2, (p2.y + 300) /2);
-		vertex(p2.x - 250, p2.y);
-		vertex((p1.x + p2.x - 410)/2, (p2.y + p1.y + 50)/ 2);
-		vertex(p1.x -160, p1.y + 50);
-		vertex(p4.x - 400, 200);
-		vertex(p4.x - 500, 250);
-	endShape();
-
-//draw clouds
+function drawCloud1(){
 	fill(255, 255, 255);
 	variationclouds1x += 1;
 	constrain(x, 0, 800);
@@ -98,16 +110,30 @@ function draw(){
 	ellipse(variationclouds1x - 100, cloud1.y + 150, cloud1.width + 30, cloud1.height + 30);
 	ellipse(variationclouds1x - 50, cloud1.y + 150, cloud1.width + 15, cloud1.height + 15);
 	ellipse(variationclouds1x, cloud1.y + 150, cloud1.width, cloud1.height);
+}
 
-//draw first mountain on the left
+function drawSecondMountainLeft(){
+	fill(17, 48, 16);
+	beginShape();
+		vertex((p2.x - 250 + p4.x - 650) / 2, (p2.y + 300) /2);
+		vertex(p2.x - 250, p2.y);
+		vertex((p1.x + p2.x - 410)/2, (p2.y + p1.y + 50)/ 2);
+		vertex(p1.x -160, p1.y + 50);
+		vertex(p4.x - 400, 200);
+		vertex(p4.x - 500, 250);
+	endShape();
+}
+
+function drawFirstMountainLeft(){
 	fill(36, 100, 34);
 	beginShape();
 		vertex(p7.x, p7.y);
 		vertex(p2.x - 250, p2.y);
 		vertex(p4.x - 650, 300);
 	endShape();
+}
 
-//draw small trees behind
+function drawSmallTreesBehind(){
 	fill(135, 86, 13);
 	ellipse(trunk.x + 100, trunk.y, trunk.width - 17, trunk.height -125);
 	ellipse(trunk.x + 110, trunk.y + 5, trunk.width - 17, trunk.height -125);
@@ -126,10 +152,10 @@ function draw(){
 	triangle(smallbranch.x1 + 25, smallbranch.y1, smallbranch.x2 + 25, smallbranch.y2, smallbranch.x3 + 25, smallbranch.y3);
 	triangle(smallbranch.x1 + 30, smallbranch.y1 - 5, smallbranch.x2 + 30, smallbranch.y2 - 5, smallbranch.x3 + 30, smallbranch.y3 - 5);
 	triangle(smallbranch.x1 + 35, smallbranch.y1 - 7, smallbranch.x2 + 35, smallbranch.y2 - 7, smallbranch.x3 + 35, smallbranch.y3 - 7);
+}
 
-
-//draw right side of the road
-	fill(48,127,72);
+function rightSideOfTheRoad(){
+		fill(48,127,72);
 	beginShape();
 		vertex(p1.x, p1.y);
 		vertex(p2.x, p2.y);
@@ -137,8 +163,9 @@ function draw(){
 		vertex(p4.x, p4.y);	
 		vertex(p5.x, p5.y);	
 	endShape();
+}
 
-//draw left side of the road
+function leftSideOfTheRoad(){
 	beginShape();
 		vertex(p2.x - 250, p2.y);
 		vertex(p3.x - 250, p3.y);
@@ -147,8 +174,9 @@ function draw(){
 		vertex(p6.x, p6.y);
 		vertex(p7.x, p7.y);
 	endShape();
+}
 
-//draw road
+function drawRoad(){
 	fill(185, 176, 93);
 	beginShape();
 		vertex(p2.x, p2.y);
@@ -159,36 +187,40 @@ function draw(){
 		vertex(p2.x - 250, p2.y);
 		vertex(p1.x -160, p1.y + 50);
 	endShape();
+}
 
-//draw the big tree
-	fill(135, 86, 13);
+function drawBigTree(){
+		fill(135, 86, 13);
 	ellipse(trunk.x, trunk.y, trunk.width, trunk.height);
 	fill(77, 178, 80)
 	triangle(branch.x1, branch.y1, branch.x2, branch.y2, branch.x3, branch.y3);
+}
 
+function scaleCharacterSmaller(){
 
-
-	//scale person smaller as he walks (à revoir)
 	push();
-	scale(0.7 * characterPosition);
+	translate(translateCharacterX, characterPositionY)
+
+	push();
+	scale(1 * characterScale);
 
 	//draw character (à revoir)
 	push();
 	//head
 	fill(102, 50, 0);
-	ellipse(translateCharacterX, translateCharacterY, 20, 25);
+	ellipse(0, 0, 20, 25);
 	//neck
-	ellipse(translateCharacterX, translateCharacterY + 12, 3, 10);
+	ellipse(0, 0 + 12, 3, 10);
 	//trunk
 	fill(129, 73, 144);
-	ellipse(translateCharacterX, translateCharacterY + 40, 16, 50);
+	ellipse(0, 40, 16, 50);
 	pop();
+	
 
 	push();
 	//right arm
 	fill(102, 50, 0);
-	ellipseMode(CENTER);
-	translate(translateCharacterX - 100, translateCharacterY + 45);
+	translate(- 100, 45);
 	rotate(75);
 	ellipse(109, 40, 7, 50);
 	//left arm
@@ -198,12 +230,14 @@ function draw(){
 
 	push();
 	fill(0, 0, 0);
-	translate(translateCharacterX - 100, translateCharacterY + 45);
 	//right leg
+	translate(- 100, 45);
+
 	push();
 	rotate(25);
 	rect(95, 27, 10, 60);
 	pop();
+
 	//left leg
 	push();
 	fill(0, 0, 0);
@@ -211,27 +245,5 @@ function draw(){
 	rect(95, -15, 10, 60);
 	pop();
 	pop();
-
-}
-
-function mouseClicked () {
-
-	fill(250, 250, 250);
-
-	translateCharacterX = constrain(translateCharacterX, 400, 520);
-	translateCharacterX -= 10;
-	if (translateCharacterX <= 400){
-		translateCharacterX = 400;
-	}
-	translateCharacterY = constrain(translateCharacterY, 400, 600);	
-	translateCharacterY -= 10;
-	if (translateCharacterY <= 400){
-		translateCharacterY = 400;
-	}
-
-	characterPosition = constrain(characterPosition, 0, 1);
-	characterPosition -= 0.01;
-
-	
-
+	pop();
 }
